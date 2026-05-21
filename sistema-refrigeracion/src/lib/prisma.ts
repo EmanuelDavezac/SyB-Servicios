262f663261294734c866f0aec8617de53074026f
Alejo Bonfanti <alejobonfanti61@gmail.com>
2026-05-20 21:39:43 -0300

    Configuración de Prisma y conexión a Neon

---
+++ sistema-refrigeracion/src/lib/prisma.ts
@@ -0,0 +1,13 @@
+// src/lib/prisma.ts (o lib/prisma.ts)
+    import { PrismaClient } from '@prisma/client';
+
+    const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+    export const prisma =
+      globalForPrisma.prisma ||
+      new PrismaClient({
+        log: ['query'], // Opcional: te muestra en la terminal las consultas SQL que se ejecutan
+      });
+
+    if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+    
